b94e9ddee33e19b4cb42d97b99238ae4d751568a
Noopur-Shriwas <noopur1224.shriwas@gmail.com>
2024-10-18 12:15:47 +0530

    first commit

---
+++ src/Pojo/Mobile.java
@@ -0,0 +1,21 @@
+package Pojo;
+
+public class Mobile {
+	
+	private String coursestitle;
+	private String price;
+	
+	public String getCoursestitle() {
+		return coursestitle;
+	}
+	public void setCoursestitle(String coursestitle) {
+		this.coursestitle = coursestitle;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+}
